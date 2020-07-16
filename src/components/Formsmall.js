812e920d1a08b32fb44412bb95f650de3966044a
Admin <admin@Picos-iMac.local>
2020-07-16 18:09:24 +0700

    update

--- src/components/Formsmall.js
+++ src/components/Formsmall.js
@@ -9,19 +9,26 @@ const Formsmall=()=>{
                 </p>
             </div>
             <div className="customer-part">
-                <div className="cust-box">
-                    <div className="cfield">
-                        <label for="name">Name</label>
-                        <input type="text" id="name" name="name"/>
-                    </div>
-                    <div className="cfield">
-                        <label for="address">Address</label>
-                        <input type="text" id="address" name="address"/>
-                    </div>
-                    <div>
-                        <button>Sign up</button> or <a href="#">Log in</a>
-                    </div>
-                </div>
+                
+
+            <form className="cust-box">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-sm btn-secondary">Submit</button>
+            </form>
+
+
             </div>
         </div>
     )
