32ac9c5566113a2d9e0734a2d8362bf595d54dc3
Admin <admin@Picos-iMac.local>
2020-07-16 17:27:40 +0700

    update

--- src/components/Formsmall.js
+++ src/components/Formsmall.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Formsmall=()=>{
     return(
         <div className="form-customer centered">
-            <div className="customer-part">
-                <p>
+            <div className="customer-part customer-desc">
+                <p className="desc-box">
                 No, you can't use the local storage of one domain to other domain. Local Storage is domain based. You can’t read or write from localstorage that’s on different domain even on it's subdomain. you can use it via Iframe on your subdomain. Please go through this link for detailed explanation.
                 </p>
             </div>
             <div className="customer-part">
-                <form>
+                <div className="cust-box">
                     <div>
                         <label for="name">Name</label>
                         <input type="text" id="name" name="name"/>
@@ -21,7 +21,7 @@ const Formsmall=()=>{
                     <div>
                         <button>Sign up</button> or <a href="#">Log in</a>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     )
