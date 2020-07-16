e3e13e508ef6cd54a86a657d9263bf888e1da7a1
Admin <admin@Picos-iMac.local>
2020-07-16 18:16:05 +0700

    update

--- src/components/Formsmall.js
+++ src/components/Formsmall.js
@@ -13,19 +13,19 @@ const Formsmall=()=>{
 
             <form className="cust-box">
             <div class="form-group">
-                <label for="exampleInputEmail1">Name</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label for="fname">Name</label>
+                <input type="text" class="form-control" id="fname" aria-describedby="emailHelp" />
+                <small id="emailHelp" class="form-text text-muted">Enter your name here.</small>
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
+                <label for="faddr">Address</label>
+                <input type="text" class="form-control" id="faddr" />
             </div>
             <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" class="btn btn-sm btn-secondary">Submit</button>
+            <button type="submit" class="btn btn-sm btn-secondary">Save</button>
             </form>
 
 
